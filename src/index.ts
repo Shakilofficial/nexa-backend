3ad08d20c6f191f7ce4001956f165a421131f135
ShakilOfficial <mrshakilhossainofficial@gmail.com>
2024-10-24 08:25:00 +0600

    done

--- src/index.ts
+++ src/index.ts
@@ -6,13 +6,15 @@ dotenv.config({
   path: './.env',
 });
 
+const port = process.env.PORT || 8000; 
+
 connectDB()
   .then(() => {
     app.listen(process.env.PORT || 8000, () => {
-      console.log(`🌐 Server is running at Port: ${process.env.PORT}`);
+      console.log(`🌐 Server is running at Port: ${port}`);
     });
   })
   .catch((err) => {
     console.log('MongoDB connection failed !!!', err);
-    process.exit(1); // Exit on MongoDB failure
+    process.exit(1);
   });
