4c0d18ed5a301534dafd2d5a7a6fcbf854a6bc4b
ShakilOfficial <mrshakilhossainofficial@gmail.com>
2024-11-09 09:41:52 +0600

    update project

--- src/app.ts
+++ src/app.ts
@@ -10,12 +10,7 @@ import userRoutes from './routes/userRoutes';
 
 const app = express();
 
-app.use(
-  cors({
-    origin: process.env.CORS_ORIGIN || 'http://localhost:3000',
-    credentials: true,
-  }),
-);
+app.use(cors());
 
 app.use(express.json({ limit: '16kb' }));
 app.use(express.urlencoded({ extended: true, limit: '16kb' }));
